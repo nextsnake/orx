9a9da2731ad077a8b57f7483adde0907ca881b84
VincentRPS <vincentbusiness55@gmail.com>
2023-07-20 21:23:17 +0800

    chore: add version

--- orx/__init__.py
+++ orx/__init__.py
@@ -7,6 +7,12 @@ A simple wrapper for the Discord API.
 :copyright: 2023 tag-epic
 """
 
+from typing import Literal
+
+
+__version__: Literal['0.1.0'] = '0.1.0'
+
+
 from .bot import *
 from .events import *
 from .flags import *
